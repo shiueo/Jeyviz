74991fbe0b9ccd1155d89c1cb92749d4faecdf63
shiüo <shiueo.csh@gmail.com>
2023-07-16 21:54:30 -0500

    d

--- Jeyviz.py
+++ Jeyviz.py
@@ -1,10 +1,14 @@
 import json
+import logging
 import os
+import platform
 import sys
 import discord as discord
 
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot, Context
+
+from utils.utils_logger import LoggingFormatter
 
 if not os.path.isfile("config.json"):
     sys.exit("config.json이 없습니다. 확인해주세요.")
@@ -19,5 +23,46 @@ bot = Bot(
     intents=intents,
     help_command=None
 )
+bot.config = config
+
+logger = logging.getLogger("Jeyviz_bot")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LoggingFormatter())
+# File handler
+
+file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
+)
+file_handler.setFormatter(file_handler_formatter)
+
+# Add the handlers
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+bot.logger = logger
+
+
+@bot.event
+async def on_ready():
+    bot.logger.info(f"Logged in as {bot.user.name}")
+    bot.logger.info(f"discord.py API version: {discord.__version__}")
+    bot.logger.info(f"Python version: {platform.python_version()}")
+    bot.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    bot.logger.info("-------------------")
+
+
+async def load_cogs():
+    for file in os.listdir("cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                await bot.load_extension(f"cogs.{extension}")
+                bot.logger.info(f"Loaded extension '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                bot.logger.error(f"Failed to load extension {extension}\n{exception}")
+
 
 bot.run(config["token"])
---
+++ utils/utils_logger.py
@@ -0,0 +1,33 @@
+import logging
+
+
+class LoggingFormatter(logging.Formatter):
+    # Colors
+    black = "\x1b[30m"
+    white = "\x1b[37m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    # Styles
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
+
+    def format(self, record):
+        log_color = self.COLORS[record.levelno]
+        format_s = "(white){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format_s = format_s.replace("(white)", self.white + self.bold)
+        format_s = format_s.replace("(reset)", self.reset)
+        format_s = format_s.replace("(levelcolor)", log_color)
+        format_s = format_s.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(format_s, "%Y-%m-%d %H:%M:%S", style="{")
+        return formatter.format(record)
