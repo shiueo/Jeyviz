441962a73366eff86e28e043e4e331c005069ac8
shiüo <shiueo.csh@gmail.com>
2023-07-19 11:04:20 -0500

    d

--- utils/utils_numunit.py
+++ utils/utils_numunit.py
@@ -1,2 +1,2 @@
 def number_formatter(v: str):
-    return ",".join([v[max(i - 3, 0) : i] for i in range(len(str(v)), 0, -3)][::-1])
+    return ",".join([v[max(i - 3, 0): i] for i in range(len(str(v)), 0, -3)][::-1])
