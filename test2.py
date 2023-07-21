1e426d7f1b7b8f63389665a9452e6b59133101bb
shiüo <shiueo.csh@gmail.com>
2023-07-21 01:38:03 -0500

    test

--- test2.py
+++ test2.py
@@ -1,91 +1,53 @@
-import argparse
-from random import randint
-import math
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
-def primes(n):
-    b = [True] * (n + 1)
-    ps = []
-    for p in range(2, n + 1):
-        if b[p]:
-            ps.append(p)
-            for i in range(p, n + 1, p):
-                b[i] = False
-    return ps
+def plot_colored_polygon(vertices_list, colors):
+    """
+    서로 다른 색으로 다각형 부분을 칠하는 함수
+    :param vertices_list: (list) 각 부분을 이루는 꼭지점들의 좌표를 순서대로 담은 리스트들의 리스트
+                          예: [[(x1, y1), (x2, y2), ..., (xn, yn)], ...]
+    :param colors: (list) 각 부분에 대응하는 색상을 담은 리스트
+                   예: ['red', 'green', 'blue', ...]
+    """
+    fig, ax = plt.subplots()
+
+    for vertices, color in zip(vertices_list, colors):
+        x, y = zip(*vertices)
+        poly = patches.Polygon(list(zip(x, y)), facecolor=color)
+        ax.add_patch(poly)
+
+    ax.autoscale()
+    ax.set_aspect('equal')
+    plt.xlabel('X축')
+    plt.ylabel('Y축')
+    plt.title('다각형 색칠하기')
+    plt.grid(True)
+    plt.show()
 
 
-def modular_inv(a, b):
-    if b == 0:
-        return 1, 0, a
-    q, r = divmod(a, b)
-    x, y, g = modular_inv(b, r)
-    return y, x - q * y, g
+# 다각형의 꼭지점들을 정의합니다.
+vertices_list = [
+    [(-36.07616, 46.49394), (-36.07616, 49.83463), (-33.96039, 54.06616), (-31.06513, 52.84124), (-26.55521, 53.28666),
+     (-20.25948, 38.74361)],  # 첫 번째 부분
+    [(-26.55521, 53.28666), (-17.42401, 67.1505), (-9.8754, 37.34933), (-20.25948, 38.74361)],  # 두 번째 부분
+    [(-26.55521, 53.28666), (-30.95378, 56.29328), (-30.17428, 58.5204), (-31.78895, 59.57828), (-31.9003, 62.52922),
+     (-34.29446, 63.30871),
+     (-33.62632, 64.58931), (-30.11861, 67.26186), (-34.90692, 75.89195), (-33.29225, 76.44874), (-30.11861, 75.72492),
+     (-27.39038, 75.55789),
+     (-25.66436, 74.83407), (-21.87825, 74.88975), (-17.75808, 73.21941), (-16.47748, 71.04796), (-17.42401, 67.1505),
+     (-26.55521, 53.28666)],
+    [(-17.42401, 67.1505), (-12.24595, 65.70287), (-8.40416, 63.25304), (-3.78289, 69.09923), (-0.55356, 65.8699),
+     (-2.39093, 64.81202), (-2.725, 60.69185)
+        , (-6.17704, 57.35116), (-2.89204, 57.57387), (-0.83195, 55.90353), (-9.88639, 37.33902)],
+    [(-9.88639, 37.33902), (-0.83195, 55.90353), (1.8406, 56.12625), (3.78933, 57.35116), (5.95743, 35.24787)],
+    [(3.78933, 57.35116), (5, 60), (6.57323, 60.46913), (10.08095, 61.69405), (18.31755, 55.41681),
+     (4.79316, 46.97876)],
+    [(4.79316, 46.97876), (18.3, 55.4), (18.85046, 33.49947), (5.92888, 35.23221)]
+]
 
+# 각 부분에 대응하는 색상을 정의합니다.
+colors = ['#703B2C', '#F2B7A7', '#F07D5E', '#70554D', "#BD634A", "#BD7D3A", "#F08162"]
 
-def elliptic_add(p, q, a, b, m):
-    if p[2] == 0:
-        return q
-    if q[2] == 0:
-        return p
-    if p[0] == q[0]:
-        if (p[1] + q[1]) % m == 0:
-            return 0, 1, 0
-        num = (3 * p[0] * p[0] + a) % m
-        denom = (2 * p[1]) % m
-    else:
-        num = (q[1] - p[1]) % m
-        denom = (q[0] - p[0]) % m
-    inv, _, g = modular_inv(denom, m)
-    if g > 1:
-        return 0, 0, denom
-    z = (num * inv * num * inv - p[0] - q[0]) % m
-    return z, (num * inv * (p[0] - z) - p[1]) % m, 1
-
-
-def elliptic_mul(k, p, a, b, m):
-    r = (0, 1, 0)
-    while k > 0:
-        if p[2] > 1:
-            return p
-        if k % 2 == 1:
-            r = elliptic_add(p, r, a, b, m)
-        k = k // 2
-        p = elliptic_add(p, p, a, b, m)
-    return r
-
-
-def lenstra(n, limit):
-    g = n
-    while g == n:
-        q = randint(0, n - 1), randint(0, n - 1), 1
-        a = randint(0, n - 1)
-        b = (q[1] * q[1] - q[0] * q[0] * q[0] - a * q[0]) % n
-        g = math.gcd(4 * a * a * a + 27 * b * b, n)
-    if g > 1:
-        return g
-    for p in primes(limit):
-        pp = p
-        while pp < limit:
-            q = elliptic_mul(p, q, a, b, n)
-            if q[2] > 1:
-                return math.gcd(q[2], n)
-            pp = p * pp
-    return False
-
-
-def main():
-    s = set()
-    while 1:
-        s.add(
-            lenstra(
-                801239812381249127412412412423423423423121231212412412234433000000,
-                10000,
-            )
-        )
-        if len(s) > 20:
-            break
-    print(s)
-
-
-if __name__ == "__main__":
-    main()
+# 다각형 부분을 서로 다른 색으로 칠합니다.
+plot_colored_polygon(vertices_list, colors)
